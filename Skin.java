9374986004bdf18072de80f02d85a2fce034ed77
huangtao <954221692@qq.com>
2023-02-23 11:23:43 +0800

    //冲突

--- Skin.java
+++ Skin.java
@@ -1,12 +1,11 @@
 public class Skin {
     public static void main(String[] args) {
         System.out.println("黄涛的修改");
-            //sdas
-        //黄涛冲突冲突222
-        //
-        // 合并之后我进行了再次的开发
-        // 在这里产生新的冲突
-        //我鞥性一下代码
-        System.out.println("冲突2");
+        int i = 1;
+        //wahaha
+        while(true){
+            System.out.println("22");
+        }
+
     }
 }
