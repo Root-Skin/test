0362dee96eff6b707e740ff58f9ab40dfb7471ea
huangtao <954221692@qq.com>
2023-02-23 11:09:37 +0800

    //冲突1

--- Skin.java
+++ Skin.java
@@ -1,8 +1,8 @@
 public class Skin {
     public static void main(String[] args) {
         System.out.println("黄涛的修改");
-        //sdas
-        //黄xian新的宠奴
-        //wdass
+            //sdas
+        //黄涛冲突            //在这里产生新的冲突
+            //wdass
     }
 }
