d2737fd58319f339ce36749b0c9b6f1aaa0522c3
huangtao <954221692@qq.com>
2023-02-23 11:01:55 +0800

    //黄想之前的代码

--- Skin.java
+++ Skin.java
@@ -1,6 +1,8 @@
 public class Skin {
     public static void main(String[] args) {
         System.out.println("黄涛的修改");
-        //黄桃的修改
+        //sdas
+        //黄xian新的宠奴
+        //wda
     }
 }
